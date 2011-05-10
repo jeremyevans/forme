b42b6f083079f17cad9cffe038cd2fdd1b158808
Jeremy Evans <code@jeremyevans.net>
2011-05-10 10:15:12 -0700

    Allow easy use of blocks to create nested tags in the sinatra erb support

--- lib/forme/sinatra.rb
+++ lib/forme/sinatra.rb
@@ -1,12 +1,21 @@
 module Forme
   module Sinatra
+    class Form < ::Forme::Form
+      def tag(type, attr={}, &block)
+        tag = Tag.new(type, attr)
+        if block
+          output = eval('@_out_buf', block.binding)
+          output << serializer.serialize_open(tag)
+          yield self
+          output << serializer.serialize_close(tag)
+        else
+          serialize(tag)
+        end
+      end
+    end
     module ERB
       def form(obj, attr={}, opts={}, &block)
-        f = ::Forme::Form.new(obj, opts)
-        output = eval('@_out_buf', block.binding)
-        output << f.open(attr)
-        yield f
-        output << f.close
+        Form.new(obj, opts).tag(:form, attr, &block)
       end
     end 
     Erubis = ERB
--- spec/forme_sinatra_spec.rb
+++ spec/forme_sinatra_spec.rb
@@ -9,7 +9,7 @@ class FormeSinatraTest < Sinatra::Base
   helpers Forme::Sinatra::ERB
   disable :show_exceptions
 
-  get /.*/ do
+  get '/' do
     erb <<END
 <% form([:foo, :bar], :action=>'/baz') do |f| %>
   <p>FBB</p>
@@ -18,10 +18,26 @@ class FormeSinatraTest < Sinatra::Base
 <% end %>
 END
   end
+
+  get '/nest' do
+    erb <<END
+<% form([:foo, :bar], :action=>'/baz') do |f| %>
+  <p>FBB</p>
+  <% f.tag(:div) do %>
+    <%= f.input(:first) %>
+    <%= f.input(:last) %>
+  <% end %>
+<% end %>
+END
+  end
 end
 
 describe "Forme Sinatra ERB integration" do
   specify "#form should add start and end tags and yield Forme::Form instance" do
-    FormeSinatraTest.new.call({'rack.input'=>'', 'REQUEST_METHOD'=>'GET'})[2].join.gsub(/\s+/, ' ') == '<form action="/baz"> <p>FBB</p> <input id="first" name="first" type="text" value="foo"/> <input id="last" name="last" type="text" value="bar"/> </form>'
+    FormeSinatraTest.new.call({'rack.input'=>'', 'REQUEST_METHOD'=>'GET', 'PATH_INFO'=>'/'})[2].join.gsub(/\s+/, ' ').should == '<form action="/baz"> <p>FBB</p> <input id="first" name="first" type="text" value="foo"/> <input id="last" name="last" type="text" value="bar"/> </form>'
+  end
+
+  specify "#form should add start and end tags and yield Forme::Form instance" do
+    FormeSinatraTest.new.call({'rack.input'=>'', 'REQUEST_METHOD'=>'GET', 'PATH_INFO'=>'/nest'})[2].join.gsub(/\s+/, ' ').should == '<form action="/baz"> <p>FBB</p> <div> <input id="first" name="first" type="text" value="foo"/> <input id="last" name="last" type="text" value="bar"/> </div> </form>'
   end
 end
