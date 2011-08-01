9c56e6ca5c5290e70085490b73ea513af7149947
Jeremy Evans <code@jeremyevans.net>
2011-08-01 11:38:57 -0700

    Bump version to 0.6.0

--- lib/forme/version.rb
+++ lib/forme/version.rb
@@ -1,6 +1,6 @@
 module Forme
   # Version constant, use <tt>Forme.version</tt> instead.
-  VERSION = '0.5.0'.freeze
+  VERSION = '0.6.0'.freeze
 
   # Returns the version as a frozen string (e.g. '0.1.0')
   def self.version
