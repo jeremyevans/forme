43907c44f772e5571a2b1d43d3507db8f550b816
Jeremy Evans <code@jeremyevans.net>
2012-04-27 12:38:31 -0700

    Don't require the Sequel core extensions in the specs
    The library doesn't currently use the core extensions, so this
    is just to ensure that they aren't used accidently in the future.

--- spec/sequel_plugin_spec.rb
+++ spec/sequel_plugin_spec.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), 'spec_helper.rb')
 
 require 'rubygems'
-require 'sequel'
+require 'sequel/no_core_ext'
 
 DB = Sequel.sqlite
 Sequel.default_timezone = :utc
