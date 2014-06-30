c9b1e0547ea6d87f9d526ac28062f54b7b5e2e31
Jeremy Evans <code@jeremyevans.net>
2014-06-30 08:42:48 -0700

    Fix rdoc for inputs wrappers

--- lib/forme.rb
+++ lib/forme.rb
@@ -1154,14 +1154,14 @@ module Forme
     Forme.register_transformer(:wrapper, k, TRANSFORMERS[:wrapper][v])
   end
 
-  # Default inputs_wrapper used by the library, uses a fieldset.
+  # Default inputs_wrapper used by the library, uses a <fieldset>.
   #
   # Registered as :default.
   class InputsWrapper
     Forme.register_transformer(:inputs_wrapper, :default, new)
 
-    # Wrap the inputs in a fieldset.  If the :legend
-    # option is given, add a +legend+ tag as the first
+    # Wrap the inputs in a <fieldset>.  If the :legend
+    # option is given, add a <legend> tag as the first
     # child of the fieldset.
     def call(form, opts)
       attr = opts[:attr] ? opts[:attr].dup : {}
@@ -1177,61 +1177,61 @@ module Forme
     end
   end
 
-  # Use a fieldset and an ol tag to wrap the inputs.
+  # Use a <fieldset> and an <ol> tag to wrap the inputs.
   #
   # Registered as :fieldset_ol.
   class InputsWrapper::FieldSetOL < InputsWrapper
     Forme.register_transformer(:inputs_wrapper, :fieldset_ol, new)
 
-    # Wrap the inputs in an ol tag
+    # Wrap the inputs in a <fieldset> and a <ol> tag.
     def call(form, opts)
       super(form, opts){form.tag_(:ol){yield}}
     end
   end
 
-  # Use an ol tag to wrap the inputs.
+  # Use an <ol> tag to wrap the inputs.
   #
   # Registered as :ol.
   class InputsWrapper::OL
     Forme.register_transformer(:inputs_wrapper, :ol, new)
 
-    # Wrap the inputs in an ol tag
+    # Wrap the inputs in an <ol> tag
     def call(form, opts, &block)
       form.tag(:ol, opts[:attr], &block)
     end
   end
 
-  # Use a div tag to wrap the inputs.
+  # Use a <div> tag to wrap the inputs.
   #
   # Registered as :div.
   class InputsWrapper::Div
     Forme.register_transformer(:inputs_wrapper, :div, new)
 
-    # Wrap the inputs in an ol tag
+    # Wrap the inputs in an <div> tag
     def call(form, opts, &block)
       form.tag(:div, opts[:attr], &block)
     end
   end
 
-  # Use a tr tag to wrap the inputs.
+  # Use a <tr> tag to wrap the inputs.
   #
   # Registered as :tr.
   class InputsWrapper::TR
     Forme.register_transformer(:inputs_wrapper, :tr, new)
 
-    # Wrap the inputs in an ol tag
+    # Wrap the inputs in an <tr> tag
     def call(form, opts, &block)
       form.tag(:tr, opts[:attr], &block)
     end
   end
 
-  # Use a table tag to wrap the inputs.
+  # Use a <table> tag to wrap the inputs.
   #
   # Registered as :table.
   class InputsWrapper::Table
     Forme.register_transformer(:inputs_wrapper, :table, new)
 
-    # Wrap the inputs in a table tag.
+    # Wrap the inputs in a <table> tag.
     def call(form, opts, &block)
       attr = opts[:attr] ? opts[:attr].dup : {}
       form.tag(:table, attr) do
