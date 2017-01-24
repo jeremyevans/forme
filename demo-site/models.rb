b131609eeff272675cb267e88fc880575cddf334
Jeremy Evans <code@jeremyevans.net>
2017-01-24 12:57:14 -0800

    Remove use of prepared_statements* plugins

--- demo-site/models.rb
+++ demo-site/models.rb
@@ -16,8 +16,6 @@ Model.db = DB
 Model.plugin :defaults_setter
 Model.plugin :forme
 Model.plugin :association_pks
-Model.plugin :prepared_statements
-Model.plugin :prepared_statements_associations
 Dir[::File.expand_path('../models/*.rb',  __FILE__)].each{|f| require f}
 
 def DB.reset
