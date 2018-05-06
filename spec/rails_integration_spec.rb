fe9fd25103a9773d580ec6323571c22d08a87014
Jeremy Evans <code@jeremyevans.net>
2018-05-05 17:28:15 -0700

    Fix deprecation warning when running tests with Rails 5.2

--- spec/rails_integration_spec.rb
+++ spec/rails_integration_spec.rb
@@ -23,7 +23,7 @@ class FormeRails < Rails::Application
   config.middleware.delete(ActionDispatch::ShowExceptions)
   config.middleware.delete(Rack::Lock)
   config.secret_key_base = 'foo'*15
-  config.secret_token = 'secret token'*15
+  config.secret_token = 'secret token'*15 if Rails.respond_to?(:version) && Rails.version < '5.2' 
   config.eager_load = true
   initialize!
 end
