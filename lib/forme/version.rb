99a7eb2692baee170ffb16b1e1da785c1f148c93
Jeremy Evans <code@jeremyevans.net>
2018-06-11 11:16:01 -0700

    Add MAJOR/MINOR/TINY/VERSION_NUMBER constants

--- lib/forme/version.rb
+++ lib/forme/version.rb
@@ -1,8 +1,22 @@
 # frozen-string-literal: true
 
 module Forme
+  # The major version of Forme, updated only for major changes that are
+  # likely to require modification to apps using Forme.
+  MAJOR = 1
+
+  # The minor version of Forme, updated for new feature releases of Forme.
+  MINOR = 7
+
+  # The patch version of Forme, updated only for bug fixes from the last
+  # feature release.
+  TINY = 0
+
   # Version constant, use <tt>Forme.version</tt> instead.
-  VERSION = '1.7.0'.freeze
+  VERSION = "#{MAJOR}.#{MINOR}.#{TINY}".freeze
+
+  # The full version of Forme as a number (1.8.0 => 10800)
+  VERSION_NUMBER = MAJOR*10000 + MINOR*100 + TINY
 
   # Returns the version as a frozen string (e.g. '0.1.0')
   def self.version
