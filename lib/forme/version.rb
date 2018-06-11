f8fdb071b9ebf45962d74117ca3ca4a58a903a68
Jeremy Evans <code@jeremyevans.net>
2018-06-11 11:30:50 -0700

    Bump version to 1.8.0

--- lib/forme/version.rb
+++ lib/forme/version.rb
@@ -6,7 +6,7 @@ module Forme
   MAJOR = 1
 
   # The minor version of Forme, updated for new feature releases of Forme.
-  MINOR = 7
+  MINOR = 8
 
   # The patch version of Forme, updated only for bug fixes from the last
   # feature release.
