0d7e76e6746c2a5bb5951c29130775ab2b5180a6
Jeremy Evans <code@jeremyevans.net>
2022-11-15 08:01:47 -0800

    Bump version to 2.3.0

--- lib/forme/version.rb
+++ lib/forme/version.rb
@@ -6,7 +6,7 @@ module Forme
   MAJOR = 2
 
   # The minor version of Forme, updated for new feature releases of Forme.
-  MINOR = 2
+  MINOR = 3
 
   # The patch version of Forme, updated only for bug fixes from the last
   # feature release.
